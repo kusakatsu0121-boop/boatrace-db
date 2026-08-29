24f7912b46c5dede77ef563c2f29fc8206035d6b
kusakatsu0121-boop <kusakatsu0121@gmail.com>
2026-08-29 16:47:36 +0900

    Fix V8 path index compatibility

---
+++ run_v8_interpretable.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import core_feature_ablation_v7 as a
+
+a.m.PATH_TO_IDX = a.be.PATH_TO_IDX
+
+import v8_interpretable_model as v8
+
+if __name__ == '__main__':
+    v8.main()
